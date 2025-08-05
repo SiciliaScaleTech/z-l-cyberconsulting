ea1d6431cdc110f9fb3b949098375ec3220a2756
Sicilia Toury <sicilia.scaletech@gmail.com>
2025-08-05 22:13:44 +0200

    création du site

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php include "header.php" ?>
+
+        <div class="slogan"><h1>Protégez vos données.<br> Sécurisez votre avenir.</h1></div>  
+
+        <main class="services">
+            <h2 class="nos_services">Nos services</h2>
+
+            <div class="accueil-container">
+                <section class="article">
+                    <a class="article-img"><img src="images/Logo - Audit Inverse.png" alt="icone Audit de sécurité" class="icone-accueil"></a>
+                    <div class="ensemble_titre_para">
+                        <h3 class="titre_service"><a href="audit.php">Audit</a></h3>
+                            <p>Notre processus d’audit de cybersécurité vous permet d’évaluer votre conformité à différents référentiels,
+                            normes de sécurité, lois et réglementations en vigueur. Nous vous aidons à prioriser les actions correctrices à mettre en œuvre pour optimiser
+                            votre cybersécurité et contrer les menaces potentielles.
+                            </p>
+                    </div>
+                <hr>
+                </section>
+                
+                <section class="article">
+                    <a class="article-img"><img src="images/Logo-Testversion-inverse.png" alt="Test d'intrusion" class="icone-accueil"></a>
+                    <div class="ensemble_titre_para">
+                        <h3 class="titre_service"><a href="pentest.php">Test d'intrusion</a></h2>
+                            <p>Une des finalités de votre activité d’indépendant est de vendre vos services. 
+                                Cela passe par la rédaction de textes selon la méthode PAS ou AIDA. 
+                                Mais au début, pour éviter l’angoisse de la page blanche, il faut faut réaliser une petit travail préalable
+                                : identifier les caractéristiques de votre offre et les associer avec le bénéfice (et les sous-bénéfices) apportés au client.
+                            </p>
+                    </div>
+                <hr>    
+                </section>
+
+                <section class="article">
+                    <a class="article-img"><img src="images/Logo - Formation inverse.png" alt="Formation" class="icone-accueil"></a>
+                    <div class="ensemble_titre_para">
+                        <h3 class="titre_service"><a href="formation.php">Formation</a></h3>
+                            <div class="formation">
+                                <p>
+                                    Formation cybersécurité et développement web/web mobile disponible pour particuliers et professionnels - <a href="contact.php" class="ensavoirplus">En savoir plus</a>
+                                </p>                  
+                            </div> 
+                    </div>
+                <hr>
+                </section>
+
+                <section class="article">
+                    <a class="article-img"><img src="images/Logo - Dev Web inverse.png" alt="Developpement" class="icone-accueil"></a>
+                    <div class="ensemble_titre_para">
+                        <h3 class="Developpement"><a href="developpement.php">Développement Web</a></h3>
+                            <div class="scaletech">
+                                <p>
+                                    Besoin de visibilité ? une idée d'application web en tête ? faites appel à notre agence web <a href="developpement.php" class="visite_scaletech">zl.ScaleTech.com</a> 
+                                </p>
+                            </div> 
+                    </div>
+                </section>     
+            </div>              
+        </main> 
+
+<?php include "footer.php" ?> 
+
+
+    
