5a69245443d92b3eec76a6f7e6462b71e8901330
Sicilia Toury <sicilia.scaletech@gmail.com>
2025-08-05 22:39:48 +0200

    page audit

---
+++ audit.php
@@ -0,0 +1,49 @@
+<?php include "header.php" ?>
+
+    <main class="audit_main">
+            <h1 class="title-audit">Les Audits</h1>
+            
+            <div class="audit-container">
+                <section class="audit-article">
+                    <div class="image-audit-pentest">
+                        <img src="images/img-services/audit-vulnérabilité.jpg" alt="image de audit de vulnérabilité" class="audit-img-droite">
+                    </div> 
+                    <div class="content">
+                        <h2 class="audit-title">Audit de Vulnérabilité</h2>
+                            <p>
+                                Un audit de vulnérabilité permet d'évaluer l’étanchéité des informations inscrites sur un support amovible ou sur un périphérique (laptop, smartphone, tablette). 
+                                Cet audit simule principalement l’accès à un périphérique par un attaquant externe, par exemple, à l’issu d’un vol ou d’une intrusion physique dans des locaux d’entreprise
+                            </p>
+                    </div>   
+                </section>
+                <hr>
+                <section class="audit-article-inverse">
+                    <div class="image-audit-pentest">
+                        <img src="images/img-services/audit-dexposition.jpg" alt="image audit d'exposition" class="audit-img-gauche">
+                    </div>
+                    <div class="content">
+                        <h2 class="audit-title">Audit d'Exposition</h2>
+                            <p>
+                                Un audit de reconnaissance permet d’identifier la surface d’attaque d’une entreprise, c’est-à-dire les éléments de son système d’information exposés sur le web.
+                                L’objectif est de dresser une cartographie claire de ces éléments afin de mieux contrôler et limiter les informations accessibles aux attaquants.
+                                Cet audit permet aussi de définir les priorités en matière de sécurité et de préparer les futurs tests d’intrusion.
+                            </p>
+                    </div>
+                </section>
+                <hr>
+                <section class="audit-article">
+                    <div class="image-audit-pentest">
+                        <img src="images/img-services/audit-AD.jpg" alt="image AD" class="audit-img-droite">
+                    </div>
+                    <div class="content">
+                        <h2 class="audit-title">Audit d'Active Directory</h2>
+                            <p>
+                                Un audit de sécurité Active Directory permet d'identifier les vulnérabilités potentielles, de garantir la conformité aux régulations en vigueur et d'améliorer les performances globales de votre système.
+                                le resultat est un rapport d'audit complet présentant les failles de votre AD.
+                            </p>
+                    </div>
+                </section>
+            </div>    
+    </main>            
+
+<?php include "footer.php" ?>            
