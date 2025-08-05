e49e88151e290f60a9c126f36ad2f15bc99a07d0
Sicilia Toury <sicilia.scaletech@gmail.com>
2025-08-05 22:42:12 +0200

    page formation

---
+++ formation.php
@@ -0,0 +1,25 @@
+<?php include "header.php" ?>
+
+<main>
+    <h1 class="service-title">Formation en CyberSécurité et Développement</h1>
+        <article class="services-article">
+                    <img src="images/img-services/pentest-web.jpg" alt="image pentest web" class="services-img-gauche">
+                        <h2 class="article-title">Pentest (Test d'intrusion) Web</h2>
+                        <p>
+                            L’objectif d’un pentest web est d’évaluer la robustesse de votre plateforme web : serveurs, applications front/back offices, webservices et APIs.
+                            Le résultat est un rapport de pentest complet permettant aux développeurs de comprendre, et de corriger les failles identifiées. 
+                            Pour les éditeurs de solutions qui souhaitent fournir des livrables à leurs clients ou partenaires, ZLCyberConsulting peut produire un deuxième rapport attestant de la correction des failles de sécurité.
+                        </p>
+                </article>  
+                <hr> 
+                <article class="services-article">
+                    <img src="images/img-services/pentest-infrastructure.jpg" alt="image pentest infrastructure" class="services-img-droite">
+                        <h2 class="article-title">Pentest (Test d'intrusion) d'Infrastructure</h2>
+                        <p>
+                            L’objectif d’un pentest d’infrastructure ou de réseau est de tester la sécurité des éléments attaquables depuis l’extérieur (IPs, serveurs) ou depuis l’intérieur de l’entreprise (serveurs, postes de travail, périphériques réseaux).
+                            Le résultat est un rapport d’audit présentant les vulnérabilités identifiées ainsi que des pistes opérationnelles pour les corriger.
+                        </p>
+                </article>   
+</main>
+
+<?php include "footer.php" ?>
