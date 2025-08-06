33e3cd701e11da2df7b246c18216f393c24d0721
Sicilia Toury <sicilia.scaletech@gmail.com>
2025-08-06 21:59:18 +0200

    ajout du dossier include

---
+++ include/header.php
@@ -0,0 +1,12 @@
+  <!DOCTYPE html>
+  <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Z&L CyberConsulting</title>
+        <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Serif:opsz,wght@8..144,300&display=swap" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+        <link rel="stylesheet" href="public/styles/css/Styles.css">
+    </head>
+  
---
+++ include/navbar.php
@@ -0,0 +1,32 @@
+  <nav class="navbar navbar-expand-lg pb-2 align-items-center fixed-top navbar-custom">
+    <div class="container-fluid">
+      <a href="index.php" class="logo"><img src="images/logo1.png" alt="Logo_zlcyberconsulting" class="img-logo"></a>
+
+      <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active text-light" aria-current="page" href="index.php">Accueil</a>
+          </li>
+          <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-light" href="services.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Services
+            </a>
+            <ul class="dropdown-menu text-light">
+              <li><a class="dropdown-item" href="audit.php">Audit</a></li>
+              <li><a class="dropdown-item" href="pentest.php">Pentest/Test d'intrusion</a></li>
+              <li><a class="dropdown-item" href="developpement.php">Développement web</a></li>
+            </ul>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link active text-light" aria-current="page" href="apropos.php">A Propos</a>
+          </li>
+          <li class="nav-item ">
+            <a class="nav-link active text-light" aria-current="page" href="contact.php">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
